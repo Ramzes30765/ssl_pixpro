9acac826fc220e8e8fd04979054746dd3f8b712d
Ramzes <ramzes30765@yandex.ru>
2025-06-25 17:49:26 +0300

    relu in projector now optional

--- src/modules.py
+++ src/modules.py
@@ -1,5 +1,42 @@
 import torch.nn as nn
 
+def _init_conv(m: nn.Module):
+    """Kaiming init conv weights as in original PixPro implementation."""
+    if isinstance(m, nn.Conv2d):
+        nn.init.kaiming_normal_(m.weight, mode="fan_out")
+        if m.bias is not None:
+            nn.init.zeros_(m.bias)
+    elif isinstance(m, (nn.BatchNorm2d, nn.SyncBatchNorm)):
+        nn.init.ones_(m.weight)
+        nn.init.zeros_(m.bias)
+
+class Projector(nn.Module):
+    def __init__(self, input_dim, proj_dim, num_blocks=1):
+        
+        super(Projector, self).__init__()
+        self.input_conv = nn.Conv2d(input_dim, proj_dim, use_relu=False, kernel_size=1)
+        if self.use_relu:
+            self.projector = nn.Sequential(
+                *[nn.Sequential(
+                    nn.Conv2d(proj_dim, proj_dim, kernel_size=1),
+                    nn.BatchNorm2d(proj_dim),
+                    nn.ReLU(inplace=True),
+                    nn.Conv2d(proj_dim, proj_dim, kernel_size=1)
+                ) for _ in range(num_blocks)]
+            )
+        else:
+            self.projector = nn.Sequential(
+                *[nn.Sequential(
+                    nn.Conv2d(proj_dim, proj_dim, kernel_size=1),
+                    nn.BatchNorm2d(proj_dim)
+                ) for _ in range(num_blocks)]
+            )
+        self.apply(_init_conv)
+    def forward(self, x):
+        x = self.input_conv(x)
+        return self.projector(x)
+    
+    
 class Predictor(nn.Module):
     def __init__(self, proj_dim, hidden_dim, num_blocks=1):
         
@@ -12,25 +49,6 @@ class Predictor(nn.Module):
                 nn.Conv2d(hidden_dim, proj_dim, kernel_size=1)
             ) for _ in range(num_blocks)]
         )
-    
+        self.apply(_init_conv)
     def forward(self, x):
-        return self.predictor(x)
-
-
-class Projector(nn.Module):
-    def __init__(self, input_dim, proj_dim, num_blocks=1):
-        
-        super(Projector, self).__init__()
-        self.input_conv = nn.Conv2d(input_dim, proj_dim, kernel_size=1)
-        self.predictor = nn.Sequential(
-            *[nn.Sequential(
-                nn.Conv2d(proj_dim, proj_dim, kernel_size=1),
-                nn.BatchNorm2d(proj_dim),
-                nn.ReLU(inplace=True),
-                nn.Conv2d(proj_dim, proj_dim, kernel_size=1)
-            ) for _ in range(num_blocks)]
-        )
-    
-    def forward(self, x):
-        x = self.input_conv(x)
         return self.predictor(x)
